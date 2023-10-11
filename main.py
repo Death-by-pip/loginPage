b5e911d994e9efd0642cc0e79d9f4f11add46063
Death-by-pip <innerprotext@gmail.com>
2023-10-11 12:49:21 -0500

    Linked buttons and added to first question

--- main.py
+++ main.py
@@ -10,6 +10,93 @@ from kivy.uix.relativelayout import RelativeLayout
 Builder.load_file("QuizPage.kv")
 
 
+class Login(Screen):
+    def verify(self):
+        with open("logins.txt", "r") as f:
+            for line in f:
+                if line.split(" ")[0] == self.ids.username.text:
+                    if line.split(" ")[1] == self.ids.password.text:
+                        self.manager.current = "one"
+                    else:
+                        self.invalid("Invalid password")
+        self.invalid("Username not found")
+
+    def invalid(self, message):
+        self.ids.invalid_txt.text = message
+
+    def register(self):
+        self.manager.current = "new"
+
+
+class Register(Screen):
+    def back(self):
+        self.manager.current = "login"
+    def verify(self):
+        if self.ids.username.text=="":
+            self.invalid("Enter Username")
+            return
+        if self.ids.pass1.text!=self.ids.pass2.text:
+            self.invalid("Passwords must match")
+            return
+        txt = self.ids.pass1.text
+        if len(txt)<8:
+            self.invalid("Password is of insufficient length")
+        LIST = []
+        check = False
+        for i in txt:
+            list_ = "a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")
+            [LIST.append(n) for n in list_]
+            if i in list_:
+                check = True
+                break
+        if check:
+            check = False
+            for i in txt:
+                list_ = "a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".upper().split(",")
+                [LIST.append(n) for n in list_]
+                if i in list_:
+                    check = True
+                    break
+        if check:
+            check = False
+            for i in txt:
+                list_ = "1,2,3,4,5,6,7,8,9,0".split(",")
+                [LIST.append(n) for n in list_]
+                if i in list_:
+                    check = True
+                    break
+        if check:
+            check = False
+            for i in txt:
+                list_ = "` ~ , . < > / ? ; ' : \" [ ] { } \\ | ! @ # $ % ^ & * ( ) - _ = +".split(" ")
+                [LIST.append(n) for n in list_]
+                if i in list_:
+                    check = True
+                    break
+        if not check:
+            self.invalid("password has insufficient characters")
+        check = True
+        for i in txt:
+            if not (i in LIST):
+                self.invalid("invalid character used")
+                return
+        for i in self.ids.username.text:
+            if not (i in LIST):
+                self.invalid("invalid character used")
+                return
+        with open("logins.txt","r") as f:
+            for line in f:
+                if line.split(" ")[0]==self.ids.username.text:
+                    self.invalid("Username already in use")
+                    return
+        with open("logins.txt","a") as f:
+            f.write("\n"+self.ids.username.text+" "+txt)
+        self.manager.current = "login"
+
+    def invalid(self, message):
+        self.ids.invalid_txt.text = message
+
+
 class QuizPageApp(App):
     def build(self):
         return QuizManager()
@@ -28,6 +115,11 @@ class Question1(Screen):
 
 
 class Question2(Screen):
+    def answered(self,correctness):
+        if correctness:
+            self.manager.current = "win"
+        else:
+            self.manager.current = "no"
     pass
 
 
@@ -38,7 +130,7 @@ class Correct(Screen):
 
 class Incorrect(Screen):
     def proceed(self):
-        self.manager.current = "two"
+        self.manager.current = "login"
 
 
 QuizPageApp().run()
